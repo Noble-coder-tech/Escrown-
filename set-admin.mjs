e3988af41e2ceec7dae1f368503d14d67858474e
www-veladownload <darkgrowth72@gmail.com>
2026-09-11 19:33:31 +0100

    Remove old files

--- set-admin.mjs
+++
@@ -1,7 +0,0 @@
-import admin from "firebase-admin";
-
-const uid = process.env.ADMIN_UID;
-if (!uid) throw new Error("Set ADMIN_UID first.");
-admin.initializeApp({ credential: admin.credential.applicationDefault() });
-await admin.auth().setCustomUserClaims(uid, { admin: true });
-console.log(`Admin claim assigned to ${uid}. The user should sign out/in or refresh their ID token.`);
